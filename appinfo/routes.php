45d4f0bedec71bc954472b544f31974666fd1db7
Happyfeet01 <3295104+Happyfeet01@users.noreply.github.com>
2026-08-26 13:25:21 +0200

    fix: route folder browsing through dedicated controller

--- appinfo/routes.php
+++ appinfo/routes.php
@@ -6,7 +6,7 @@ return [
 	'routes' => [
 		['name' => 'library#settings', 'url' => '/api/settings', 'verb' => 'GET'],
 		['name' => 'library#saveSettings', 'url' => '/api/settings', 'verb' => 'POST'],
-		['name' => 'library#folders', 'url' => '/api/folders', 'verb' => 'GET'],
+		['name' => 'folder#folders', 'url' => '/api/folders', 'verb' => 'GET'],
 		['name' => 'library#scan', 'url' => '/api/library/scan', 'verb' => 'POST'],
 		['name' => 'library#musicBrainz', 'url' => '/api/musicbrainz', 'verb' => 'GET'],
 		['name' => 'library#previewMove', 'url' => '/api/library/preview-move', 'verb' => 'POST'],
